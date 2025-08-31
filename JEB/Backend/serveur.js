083681fa19d9f56a504a6db7cf8aad13fa256d8b
yoann;^) <yoann.dossou@epitech.eu>
2025-08-31 17:38:24 +0100

    configuration du serveur et des midleswares, fichier db.js pour la base de données et dossier pour les routes

--- JEB/Backend/serveur.js
+++ JEB/Backend/serveur.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./db");
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.send("Backend Working!");
 });
+
+const authRoutes = require("./routes/authentification")
+app.use("auth", authRoutes);
 
 app.listen(port, () => {
     console.log(`Serveur en écoute sur le port: ${port}`);
